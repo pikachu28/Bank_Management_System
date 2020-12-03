0380ac9feb54e4e86f0e709e346a6e068eaa417c
Binwant Kaur <57220065+kbinwant31@users.noreply.github.com>
2020-12-03 16:50:29 +0530

    Add files via upload

---
+++ emp.h
@@ -0,0 +1,23 @@
+//employee class
+
+#include"customer.h"
+
+class employee
+{
+private:
+   int emp_id;
+   std::string empName="";
+   std::string empPassword="";
+   std::string desig="";
+   std::string DOB="";
+   std::string phone_no="";
+
+public:
+   employee();
+   employee(int emp_id,std::string empName,std::string desig);
+   ~employee();
+   void VeiwAllAccounts(); // to view all user accounts from the db.-*(cust_name and account_no only.)*
+   void SearchCustomer(); //to search for a customer details using cust_id or acc_no
+   void updateDetails(); //to update name/phone no./DOB/Designation-(not password) of self
+   void updateCust_Details(); //to update customer details- **for managers only**
+};
