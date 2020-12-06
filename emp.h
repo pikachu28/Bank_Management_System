069543d2768a733c8b213aa394760579a2aa8b39
Mitali-laroia <04042001mitali@gmail.com>
2020-12-06 11:48:48 +0530

    function to get record of employee added

--- emp.h
+++ emp.h
@@ -18,6 +18,23 @@ public:
    employee();
    employee(int emp_id,std::string empName,std::string desig);
    ~employee();
+
+   void getRecord(){
+      cout<<"\n\n Enter Employee name.";
+      cin>>emp_name;
+      cout<<"\n\n Enter Password for your login account";
+      cin>>empPassword;
+      cout<<"\n\n Enter employee address";
+      cin>>emp_add;
+      cout<<"\n\n Enter employee email";
+      cin>>emp_email;
+      cout<<"\n\n Enter designation of the employee";
+      cin>>desig;
+      cout<<"Enter the date of birth";
+      cin>>DOB;
+      cout<<"Enter mobile no. of the employee";
+      cin>>emp_moblie;
+   }
    void VeiwAllAccounts(); // to view all user accounts from the db.-*(cust_name and account_no only.)*
    void SearchCustomer(); //to search for a customer details using cust_id or acc_no
    void updateDetails(); //to update name/phone no./DOB/Designation-(not password) of self
