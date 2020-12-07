1cfc0ff40314461de706f169e767ed50fee1ea66
Binwant Kaur <57220065+kbinwant31@users.noreply.github.com>
2020-12-07 16:11:25 +0530

    Update db.h

--- db.h
+++ db.h
@@ -1,23 +1,23 @@
 #pragma once
-#include"emp.h"
+#include<iostream>
+#include<string>
 
 //definition of database utilisation functions
 
-class db
+class database
 {
-
 public:
-	db();
-	~db();
-	int open_db(string);
-	int print_data(string);
-	void create_db();
+	database();
+	~database();
+	int open_db();
+	//int print_data(string s);
+	//void create_db();
 	void close_db();
 	void createEmpTable();
 	//void createCustTable();
 	//void createTransactionTable();
-	void check(int);
-	void insertEmpTable(int, string, string, string, string, string, string);
+	//void check(int);
+	void insertEmpData(int empid, std::string name, std::string desig, std::string dob, std::string gender, std::string pass);
 	void printEmpData();
 
 };
