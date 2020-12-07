438169e27740ca85d71e60d298777aa59c87889a
Anagha Jain <57215051+AnaghaJain@users.noreply.github.com>
2020-12-07 18:54:25 +0530

    Update customer_menu.cpp

--- customer_menu.cpp
+++ customer_menu.cpp
@@ -1,5 +1,3 @@
-// test.cpp : This file contains the 'main' function. Program execution begins and ends there.
-
 #include<iostream>
 #include<string>
 
@@ -44,183 +42,210 @@ public:
 };
 void Cust::newAccountCUST()
 {
-        cout << "\n\n\n\tCUSTOMER DETAILS";
-        cout << "\n\n\t Enter Customer Name:";
+        cout << "\n\n\t CUSTOMER DETAILS";
+        cout << "\n\t Enter Customer Name: ";
         cin >> cust_name;
-        cout << "\n\n\t Enter Customer Email ID:";
+        cout << "\n\t Enter Customer Email ID: ";
         cin >> cust_email;
-        cout << "\n\n\t Enter Customer Address";
+        cout << "\n\t Enter Customer Address: ";
+        cin >> cust_add;
+        cout << "\n\t Enter Gender (M/F): ";
         cin >> cust_add;
 
+
         //make an account and show the details.
-        cout << "\n\n\t Your Account has been created Successfully!";
+        cout << "\n\t Your Account has been created Successfully!";
 
         cout << "\n\n\t Your default Password is 0";
-        cout << "\n\n\t CUSTOMER BANK BALANCE = 0";
+        cout << "\n\t CUSTOMER BANK BALANCE = 0";
         //bank balance can be viewed on the table only - the default password line can be removed
 
-        cout << "\n\n\t TO DEPOSIT MONEY PRESS 1";
-        cout << "\n\n\t TO CHANGE PASSWORD PRESS 2";
+        cout << "\n\n\n\t 1. TO DEPOSIT MONEY";
+        cout << "\n\t 2. TO CHANGE PASSWORD";
+        cout << "\n\t Please Enter Your Choice (1 OR 2) ";
 
         int a1;
         cin >> a1;
 
         switch (a1) {
-            case '1': clrscr();
+            case 1: clrscr();
                      deposit_withdraw();
                      break;
-            case '2': clrscr();
+            case 2: clrscr();
                      cust_updateDetails();
                      //change details
                      break;
-            case '3': break;
+            case 3: break;
         }
 
     }
 
     void Cust::cust_getRecord() {
-        cout << "\n\n\n\tCUSTOMER DETAILS";
-        cout << "\n\n\t Enter Customer Name:";
-        cin >> cust_name;
-        cout << "\n\n\t Enter Customer Id:";
-        cin >> acc_no;
-        cout << "\n\n\t Enter Password";
-        cin >> cust_pass;
+        // cout << "\n\n\tCUSTOMER DETAILS";
+        // cout << "\n\t Enter Customer Name:";
+        // cin >> cust_name;
+        // cout << "\n\t Enter Customer Id:";
+        // cin >> acc_no;
+        // cout << "\n\t Enter Password";
+        // cin >> cust_pass;
 
         //open the details table
         //can also have CUSTOMER BANK BALANCE"
 
         cout << endl;
-        cout << "\n\n\t TO UPDATE YOUR DETAILS PRESS 1";
-        cout << "\n\n\t TO DEPOSIT OR WITHDRAW MONEY PRESS 2";
-        cout << "\n\n\tPlease Enter Your Choice (1 OR 2) ";
+        cout << "\n\t 1. TO UPDATE YOUR DETAILS";
+        cout << "\n\t 2. TO DEPOSIT OR WITHDRAW MONEY";
+        cout << "\n\t Please Enter Your Choice (1 OR 2) ";
 
         int b1;
         cin >> b1;
 
         switch (b1) {
-        case '1': clrscr();
+        case 1: clrscr();
             cust_updateDetails();
             break;
-        case '2': clrscr();
+        case 2: clrscr();
             deposit_withdraw();
             break;
-        case '3': break;
+        case 3: break;
         }
     }
 
     void Cust::cust_updateDetails() {
-        cout << "\n\n\t Enter Customer Name:";
-        cin >> cust_name;
-        cout << "\n\n\t Enter Customer Id:";
-        cin >> acc_no;
-        cout << "\n\n\t Enter Password";
-        cin >> cust_pass;
+        // cout << "\n\t Enter Customer Name:";
+        // cin >> cust_name;
+        // cout << "\n\t Enter Customer Id:";
+        // cin >> acc_no;
+        // cout << "\n\t Enter Password";
+        // cin >> cust_pass;
 
+        cout << "\n\n\t CUSTOMER DETAILS";
         //cout the table of your details.
-        cout << "\n\n\n\tCUSTOMER DETAILS";
 
-        cout << "\n\n\t UPDATE YOU DETAILS";
-        cout << "\n\n\t1. To change your Address";
-        cout << "\n\n\t2. To change your Email ID";
-        cout << "\n\n\t3. To change your Phone number";
-        cout << "\n\n\t4. To change your Designation";
-        cout << "\n\n\t5. To change your Password";
-        cout << "\n\n\tPlease Enter Your Choice (1-5)";
+        cout << "\n\t UPDATE YOU DETAILS";
+        cout << "\n\t 1. To change your Address";
+        cout << "\n\t 2. To change your Email ID";
+        cout << "\n\t 3. To change your Designation";
+        cout << "\n\t 4. To change your Password";
+        // cout << "\n\t5. To change your Phone number";
+        cout << "\n\t Please Enter Your Choice (1-4)";
         cout << endl;
         int c1;
         cin >> c1;
 
         switch (c1) {
-        case '1': clrscr();
-            cout << "Enter New Address: ";
+        case 1: clrscr();
+            cout << "\n\t Enter New Address: ";
+            cin>>cust_add;
             break;
-        case '2': clrscr();
-            cout << "Enter New Email ID: ";
+        case 2: clrscr();
+            cout << "\n\t Enter New Email ID: ";
+            cin>>cust_email;
             break;
-        case '3': clrscr();
-            cout << "Enter New Phone number: ";
-            break;
-        case '4': clrscr();
+        case 3: clrscr();
+            cout << "\n\t Your designation has been updated! ";
             //here we can delete the account and make an employee account with same data
             //two dbms commands to be included
-        case '5': clrscr();
-            cout << "Enter New Password: ";
+            //3. is not working on my laptop 
+        case 4: clrscr();
+            cout << "\n\t Enter New Password: ";
+            cin>>cust_pass;
             break;
-        case '6': break;
+        // case 5: clrscr();
+        //     cout << "Enter New Phone number: ";
+        //     break;
+        case 5: break;
         }
-        cout << "\n\n\n\tUPDATED CUSTOMER DETAILS";
+        cout << "\n\n\t UPDATED CUSTOMER DETAILS";
         //cout the table of your details.
     }
 
     void Cust::deposit_withdraw() {
-        cout << "\n\n\t Enter Customer Name:";
-        cin >> cust_name;
-        cout << "\n\n\t Enter Customer Id:";
-        cin >> acc_no;
-        cout << "\n\n\t Enter Password";
-        cin >> cust_pass;
+        // cout << "\n\n\t Enter Customer Name:";
+        // cin >> cust_name;
+        // cout << "\n\n\t Enter Customer Id:";
+        // cin >> acc_no;
+        // cout << "\n\n\t Enter Password";
+        // cin >> cust_pass;
 
         //show table
-        cout << "Enter the amount you want to deposit or withdraw: ";
-        cout << "NOTE: to withdraw, use the negative sign with the amount";
-        //make changes in account details and display the updated table;
 
-        //or we can make a switch case;
+        cout << "\n\t 1. TO WITHDRAW";
+        cout << "\n\t 2. TO DEPOSIT";
+        cout << "\n\t Please Enter Your Choice (1 OR 2) ";
+
+        int b1, amount;
+        cin >> b1;
+
+        switch (b1) {
+                case 1: 
+                cout << "\n\t Enter the amount you want to withdraw: ";
+                cin>>amount;
+                //make changes in the account transaction section
+                //amount is declared locally - change it to your class
+                break;
+                case 2:
+                cout << "\n\t Enter the amount you want to deposit: ";
+                cin>>amount;
+                //make changes in the account transaction section
+                //amount is declared locally - change it to your class
+                break;
+                case 3: break;
+        }
     }
 
     void Cust::delete_acc() {
-        cout << "\n\n\n\tCUSTOMER DETAILS";
-        cout << "\n\n\t Enter Customer Name:";
-        cin >> cust_name;
-        cout << "\n\n\t Enter Customer Id:";
-        cin >> acc_no;
-        cout << "\n\n\t Enter Password";
-        cin >> cust_pass;
+        // cout << "\n\n\n\tCUSTOMER DETAILS";
+        // cout << "\n\t Enter Customer Name:";
+        // cin >> cust_name;
+        // cout << "\n\n\t Enter Customer Id:";
+        // cin >> acc_no;
+        // cout << "\n\n\t Enter Password";
+        // cin >> cust_pass;
 
         //show the account details.
 
-        cout << "\n\n\t IF YOU WISH TO DELETE THE ACCOUNT PRESS 1";
-        cout << "\n\n\t ELSE PRESS 2";
+        cout << "\n\t IF YOU WISH TO DELETE THE ACCOUNT PRESS 1";
+        cout << "\n\t ELSE PRESS 2";
+        cout << "\n\t Please Enter Your Choice (1 OR 2) ";
 
         int d1;
         cin >> d1;
 
         switch (d1) {
-        case '1': clrscr();
-            //delete the account from the db
-            break;
-        case '2': clrscr();
-            //go to the customer menu
-            break;
+                case 1: clrscr();
+                cout << "\n\t Your account has been successfully deleted";
+                //delete the account from the db
+                break;
+                case 2: clrscr();
+                //go to the customer menu
+                break;
         }
 }
-int main()
-{
+
+int main(){
    int key;
-   cout << "\n\n\n\tCUSTOMER MENU";
-   cout << "\n\n\t1.MAKE A NEW ACCOUNT";
-   cout << "\n\n\t2.DISPLAY YOUR DETAILS";
-   cout << "\n\n\t3.UPDATE YOUR DETAILS";
-   cout << "\n\n\t4.DEPOSIT OR WITHDRAW MONEY";
-   cout << "\n\n\t5.DELETE ACCOUNT";
-   cout << "\n\n\tPlease Enter Your Choice (1-5) ";
+   cout << "\n\n\t CUSTOMER MENU";
+   cout << "\n\t 1.MAKE A NEW ACCOUNT";
+   cout << "\n\t 2.DISPLAY YOUR DETAILS";
+   cout << "\n\t 3.UPDATE YOUR DETAILS";
+   cout << "\n\t 4.DEPOSIT OR WITHDRAW MONEY";
+   cout << "\n\t 5.DELETE ACCOUNT";
+   cout << "\n\t Please Enter Your Choice (1-5) ";
    cin >> key;
    Cust c1;
    switch (key) {
-      case 1: clrscr();
-          c1.newAccountCUST();
-          break;
-      case 2: c1.cust_getRecord();
-          break;
-      case 3: c1.cust_updateDetails();
-          break;
-      case 4: c1.deposit_withdraw();
-          break;
-      case 5: c1.delete_acc();
-          break;
-      case 6: break;
+        case 1: clrscr();
+                c1.newAccountCUST();
+                break;
+        case 2: c1.cust_getRecord();
+                break;
+        case 3: c1.cust_updateDetails();
+                break;
+        case 4: c1.deposit_withdraw();
+                break;
+        case 5: c1.delete_acc();
+                break;
+        case 6: break;
    }
 }
-
-
