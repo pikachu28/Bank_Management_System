0149db90f7368366c4f344894e71dbd7e478f9b3
Mitali-laroia <04042001mitali@gmail.com>
2020-12-09 18:53:34 +0530

    Create Admin_menu.cpp

---
+++ Admin_menu.cpp
@@ -0,0 +1,32 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+ 
+	int ch;
+	system("CLS");
+	cout << "\t\t\t\t    ::ADMIN MENU::\n";
+	cout << "\n\t\t\t\t1. INSERT EMPLOYEE'S RECORD";
+	cout << "\n\t\t\t\t2. UPDATE DETAILS OF AN EMPLOYEE";
+	cout << "\n\t\t\t\t3. DELETE EMPLOYEE RECORD";
+	cout << "\n\t\t\t\t4. BACK TO MAIN MENU";
+	cout << "\n\n\t\t\tEnter your option(1-4)=> ";
+	cin >> ch;
+
+	switch(ch)
+            {
+                case '1': 
+                       //function to insert a employee record
+                        break;
+                case '2': //function to update employee record
+                        break;
+                case '3':
+                       //function to delete employee record
+                        break;
+                case '4': admin_menu();
+                        break;
+                default:admin_menu();
+            }
+
+
+}
