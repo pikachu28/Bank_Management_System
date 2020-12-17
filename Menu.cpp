48b4c7851766b617e0e425cbebb630dcff7c5ad5
Anjali Singh <62153950+pikachu28@users.noreply.github.com>
2020-12-17 19:50:40 +0530

    Add files via upload

---
+++ Menu.cpp
@@ -0,0 +1,177 @@
+#include <iostream>
+#include<sqlite3.h>
+#include "employeeLogin.h"
+#include "customer.h"
+#include "customerFunction.h"
+using namespace std;
+
+void adminMenu(){
+	system("clear");
+	cout << "\n\n\t\t\tGo To Admin Login";
+	if(adminLogin()){
+		int ch;
+		cout << "\t\t\t\t    ::ADMIN MENU::\n";
+		cout << "\n\t\t\t\t1. INSERT EMPLOYEE'S RECORD";
+		cout << "\n\t\t\t\t2. UPDATE DETAILS OF AN EMPLOYEE";
+		cout << "\n\t\t\t\t3. DELETE EMPLOYEE RECORD";
+		cout << "\n\t\t\t\t4. View Employee Data";
+		cout << "\n\n\t\t\tEnter your option(1-4)=> ";
+		cin >> ch;
+	switch(ch)
+	{
+		case 1: 
+			//function to insert/register a employee record
+			empRegister();
+			break;
+		case 2: 
+			//function to update employee record
+			empUdate();
+			break;
+		case 3:
+			//function to delete employee record
+			empDel();
+			break;
+		case 4: 
+			// Funtion to view the employee data
+			empView();
+			break;
+			default:cout << "\a";
+								}
+	}
+
+}
+
+void employee_menu()
+{
+	int ch;
+	if(empLogin()){
+		do{
+	system("clear");
+	cout << "\n\n\t\t\t\t======================\n";
+	cout << "\t\t\t\tBANK MANAGEMENT SYSTEM";
+	cout << "\n\t\t\t\t======================\n";
+
+	cout << "\t\t\t\t    ::EMPLOYEE MENU::\n";
+	cout << "\n\t\t\t\t1. NEW RECORD";
+	cout << "\n\t\t\t\t2. UPDATE DETAILS";
+	cout << "\n\t\t\t\t3. VIEW CUSTOMER ACCOUNTS";
+	cout << "\n\t\t\t\t4. MODIFY AN ACCOUNT";
+	cout << "\n\t\t\t\t5. ALL ACCOUNT HOLDER LIST";
+	cout << "\n\t\t\t\t6. CLOSE AN ACCOUNT";
+	cout << "\n\t\t\t\t7. BACK TO MAIN MENU";
+	cout << "\n\n\t\t\tEnter your option(1-8)=> ";
+	cin >> ch;
+	switch(ch){
+		case 1:
+			NewAccount();
+			break;
+		case 2:
+			updateDetails();
+			break;
+		case 3:
+			viewCustDetails();
+			break;
+		case 5:
+			viewCustTable();
+			break;
+		case 6:
+			closeAcc();
+			break;
+		default:cout << "\a";
+	}
+		}while(ch!=7);
+
+	}
+
+}
+
+void cust_menu()
+{
+	int ch;
+	system("clear");
+	if(custLogin()){
+			do{		
+		cout << "\n\n\t\t\t\t======================\n";
+		cout << "\t\t\t\tBANK MANAGEMENT SYSTEM";
+		cout << "\n\t\t\t\t======================\n";
+
+		cout << "\t\t\t\t    ::CUSTOMER MENU::\n";
+		cout << "\n\t\t\t\t1. View ACCOUNT Details";
+		cout << "\n\t\t\t\t2. UPDATE DETAILS";
+		cout << "\n\t\t\t\t3. DEPOSIT";
+		cout << "\n\t\t\t\t4. WITHDRAW";
+		cout << "\n\t\t\t\t5. View Transaction Details";
+		cout << "\n\t\t\t\t6. BACK TO MAIN MENU";
+		cout << "\n\n\t\t\tEnter your option(1-5)=> ";
+		
+		cin >> ch;
+		switch (ch) {
+			case 1: 
+				viewCustDetails();
+				break;
+			case 2: 
+				updateDetails();
+				break;
+			case 3:
+				depositInBal();
+				break;
+			case 4: 
+				withdrawInBal();
+				break;
+			case 5:
+				viewTransaction();
+				break;
+			case 6:
+				break;
+		}
+	}while(ch<6);
+ }
+}
+int main()
+{
+	
+	int ch;
+	do
+	{
+		system("clear");
+		cout << "\n\n\t\t\t\t======================\n";
+		cout << "\t\t\t\tBANK MANAGEMENT SYSTEM";
+		cout << "\n\t\t\t\t======================\n";
+
+		cout << "\t\t\t\t    ::MAIN MENU::\n";
+		cout << "\n\t\t\t\t1. ADMIN";
+		cout << "\n\t\t\t\t2. EMPLOYEE";
+		cout << "\n\t\t\t\t3. CUSTOMER";
+		cout << "\n\t\t\t\t4. EXIT";
+		cout << "\n\n\t\t\tSelect Your Option (1-4): ";
+		cin >> ch;
+
+		switch (ch)
+		{
+		case 1:
+				adminMenu();
+				break;
+		case 2:
+				employee_menu();
+				break;
+		case 3:
+				system("clear");
+				cout << "\n\n\t\t\tGo to Customer Login";
+				cust_menu();
+				break;
+		case 4:
+				cout << "\n\n\t\t\t-Developed by-";
+				cout << "\n\n\t\t\tMitali Larioa(020)";
+				cout << "\n\n\t\t\tBinwant Kaur(030)";
+				cout << "\n\n\t\t\tAnagha Jain(037)";
+				cout << "\n\n\t\t\tAnjali Singh(038)";
+				cout << "\n\n\t\t\tAstha Khatana(040)";
+				break;
+		default:cout << "\a";
+		}
+		cin.ignore();
+		cin.get();
+	} while (ch < 4);
+	return 0;
+}
+
---
+++ customerFunction.h
@@ -0,0 +1,142 @@
+#include <iostream>
+#include <sqlite3.h>
+#include<string>
+#include"db.h"
+using namespace std;
+
+string password,acc, changed, i, x;
+int deposit=0;
+
+void updateBal(int d){
+    i = to_string(d);
+    sql = "UPDATE customer SET Balance = '" + i + "' WHERE AccountNo='"+ acc +"'";
+    rc = sqlite3_exec(db, sql.c_str(), callback, NULL, NULL);
+    if(rc != SQLITE_OK){
+        cout<<"Your balance is not updated"<<endl;
+        }
+        else{
+            cout<<"Your balance has been updated"<<endl;
+            tempdb.createTransactionTable(acc);
+            tempdb.TransactionTable(acc);
+        }
+    }
+
+static int callback_2(void* data, int argc, char** argv, char ** azColName){
+        string colName;
+        char* colValueString;
+        char* y;
+        for(int i=0; i<argc; i++){
+            colName = azColName[i];
+            colValueString = argv[i];
+            if(colValueString==0){
+                y = NULL;
+            }
+            else{
+                y = colValueString;
+            }
+            int x;
+            if((!colName.compare("Balance"))&(deposit!=0)){
+                x = atoi(colValueString);
+                changed = to_string(deposit);
+                deposit = x + deposit;
+                updateBal(deposit);
+            }
+        }
+        return 0;
+    }
+
+
+
+void depositInBal(){
+    cout<<"\n\t\t\tEnter the deposit: ";
+    cin>>deposit;
+    sql = "SELECT Balance FROM customer WHERE AccountNo='"+ acc +"'";
+    rc = sqlite3_exec(db, sql.c_str(), callback_2, NULL, NULL);
+    if(rc != SQLITE_OK){
+        cout<<"Error in accessing database"<<endl;
+        }
+}
+
+void withdrawInBal(){
+    int w;
+    cout<<"\n\t\t\tEnter the amount to withdraw: ";
+    cin>>w;
+    deposit = w - 2*w;
+    cout<<deposit<<endl;
+    string withdraw = to_string(w);
+    sql = "SELECT Balance FROM customer WHERE AccountNo='"+ acc +"' and Balance> '"+ withdraw +"'";
+    rc = sqlite3_exec(db, sql.c_str(), callback_2, NULL, NULL);
+
+}
+bool database::viewTransactionDetails(string acc_no){
+    x = "Transaction_"+acc_no;
+    sql = "SELECT * FROM '"+ x +"'";
+    rc = sqlite3_exec(db, sql.c_str(), callback, NULL, NULL);
+    if(rc != SQLITE_OK){
+        return false;
+        }
+     else{
+            return true;
+        }
+}
+
+void viewTransaction(){
+    if(tempdb.viewTransactionDetails(acc)){
+        //x = "Transaction_"+acc_no;
+        sql = "SELECT * FROM '"+ x +"'";
+        sqlite3_exec(db, sql.c_str(), callback, NULL, NULL);
+    }
+    else{
+        cout<<"Can not access your transaction...RETRY";
+    }
+}
+bool custLogin(){
+    cout<<"\n\t\t\tEnter your id: ";
+    cin>>acc;
+    cout<<"\n\t\t\tEnter password: ";
+    cin>>password;
+    sql = "Select *FROM customer WHERE AccountNo='"+ acc +"' and emp_password='"+ password +"'";
+    rc = sqlite3_exec(db, sql.c_str(), callback, NULL, NULL);
+    if(rc != SQLITE_OK){
+        cout<<"\n\t\t\tIncorrect Account Id or password"<<endl;
+        return false;
+        }
+        else{
+            //write whatever function u wnat to perform in this block after login
+            cout<<"\n\t\t\tLogin Sucessful";
+            return true;
+        }
+    }
+
+void database::createTransactionTable(string acc_no)
+{
+    x = "Transaction_"+acc_no;
+	sql = "CREATE TABLE IF NOT EXISTS '"+ x +"' ("
+        "TransactionID INTEGER PRIMARY KEY AUTOINCREMENT,"
+		"CURRENT_BALANCE VARCHAR(50) NOT NULL,"
+		"AMOUNT_Deposit_withdraw VARCHAR(50) NOT NULL,"
+        "DATE TEXT);";
+	rc = sqlite3_exec(db, sql.c_str(),callback, NULL, NULL);	
+	if (rc != SQLITE_OK) {
+		cout << "Error: " <<endl;
+	}
+	else{
+		cout << "Table TRANSACTION exits!";
+	}
+	}
+
+
+void database::TransactionTable(string acc_no){
+    // int acc;
+    // acc = stoi(acc_no);
+    // string a = to_string(acc);
+    x = "Transaction_"+acc_no;
+    sql = "INSERT INTO '"+ x +"'(CURRENT_BALANCE, AMOUNT_Deposit_withdraw, DATE) VALUES ('"+ i +"','"+ changed +"', datetime('now', 'localtime'));";
+    rc = sqlite3_exec(db, sql.c_str(),callback, NULL, NULL);	
+	if (rc != SQLITE_OK) {
+		cout << "Error: " <<endl;
+	}
+	else{
+		cout << "Transaction Completed";
+	}
+}
